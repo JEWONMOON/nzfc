73e779a28b8235a14cff7c6938eab2d39eea07ef
JEWONMOON <37321672+JEWONMOON@users.noreply.github.com>
2026-04-13 10:16:39 +0900

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package nzfc where
 
 -- Mathlib 버전을 Lean 버전과 일치하도록 @ "v4.11.0"을 추가합니다.
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.11.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.0"
 
 @[default_target]
 lean_lib nzfc where
